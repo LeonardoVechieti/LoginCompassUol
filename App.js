11320befea58207f15272748b212869324363dd2
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-01-29 23:29:51 -0300

    Feat: Adding pages and features

--- App.js
+++ App.js
@@ -1,56 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, Alert, ToastAndroid, Container } from 'react-native';
+//importa conteudo de textos
+import { content } from './src/data/content'
 
 export default function App() {
-  const [user, setUser] = useState(null)
+  const [context, setContext] = useState("Home")
+  const [name, setName] = useState('')
+  const [user, setUser] = useState([])
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+
+
+
   const login = () => {
+    //compara email e password
+    if (email === user.email && password === user.password) {
+      Alert.alert('Login Success')
+      //Redirect to home page
+      setContext("Home")
+    }
+  }
+  const register = () => {
     const data = {
+      name: name,
       email: email,
       password: password
     }
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(data => {
-        setUser(data.user)
-        Alert.alert('Login Success')
-      })
-      .catch(err => {
-        Alert.alert('Login Failed')
-      }
-      )
+    setUser(data)
+    Alert.alert('Register Success')
+    //Redirect to login page
+    setContext("Login")
   }
 
-  return (
-    <View style={styles.container}>
-      <Image style={{ width: 260, height: 100 }} source={require('./assets/logo2.png')} />
-      <TextInput style={styles.textInput} keyboardType="email-address" placeholder="E-mail" onChangeText={text => { setEmail(text) }}></TextInput>
-      <TextInput style={styles.textInput} secureTextEntry placeholder="Password" onChangeText={text => { setPassword(text) }} ></TextInput>
-      <TouchableOpacity>
-        <Text style={styles.button} onPress={login}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={{ color: 'orange', marginTop: 10 }}>Create Account</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={{ color: 'orange', marginTop: 10 }}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <Text style={{ color: 'gray', marginTop: 150, }}>© 2023 - All Rights Reserved</Text>
-      <Text style={{ color: 'gray', marginTop: 5, }}> Leonardo Vechieti</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+  if (context==="Login"){
+    return (
+        <View style={styles.container}>
+          <Image style={{ width: 260, height: 100 }} source={require('./assets/logo2.png')} />
+          <TextInput style={styles.textInput} keyboardType="email-address" placeholder="E-mail" onChangeText={text => { setEmail(text) }}></TextInput>
+          <TextInput style={styles.textInput} secureTextEntry placeholder="Password" onChangeText={text => { setPassword(text) }} ></TextInput>
+          <TouchableOpacity>
+            <Text style={styles.button} onPress={login}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ color: 'orange', marginTop: 10 }} onPress={()=>{setContext("Register")}}>Create Account</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={{ color: 'orange', marginTop: 10 }} onPress={()=>{setContext("Forgot Password")}}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <Text style={{ color: 'gray', marginTop: 150, }}>© 2023 - All Rights Reserved</Text>
+          <Text style={{ color: 'gray', marginTop: 5, }}> Leonardo Vechieti</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    
+  }
+  else if (context==="Register"){
+    return (
+      <View style={styles.container}>
+        <Image style={{ width: 260, height: 100 }} source={require('./assets/logo2.png')} />
+        <TextInput style={styles.textInput} placeholder="Full Name" onChangeText={text => { setName(text) }}></TextInput>
+        <TextInput style={styles.textInput} keyboardType="email-address" placeholder="E-mail" onChangeText={text => { setEmail(text) }}></TextInput>
+        <TextInput style={styles.textInput} secureTextEntry placeholder="Password" onChangeText={text => { setPassword(text) }} ></TextInput>
+        <TouchableOpacity>
+          <Text style={styles.button} onPress={register}>Register</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={{ color: 'orange', marginTop: 10 }} onPress={()=>{setContext("Login")}}>Login</Text>
+        </TouchableOpacity>
+        <Text style={{ color: 'gray', marginTop: 150, }}>© 2023 - All Rights Reserved</Text>
+        <Text style={{ color: 'gray', marginTop: 5, }}> Leonardo Vechieti</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  else if (context==="Home"){
+    return (
+      <View style={styles.home}>
+        <View style={styles.header}>
+          <Image style={{ width: 260, height: 100 }} source={require('./assets/logo2.png')} />
+        </View>
+        <Text style={styles.textHome}>Welcome {user.name}</Text>
+        <Text style={{fontSize: 20}}>{
+        }</Text>
+        <Text style={{fontSize: 20}}>Your password is {user.password}</Text>
+      </View>
+    );
+  }
+  else if (context==="Logout"){
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize: 20}}>Logout</Text>
+      </View>
+    );
+  }
+  else if (context==="Forgot Password"){
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize: 20}}>Forgot Password</Text>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -81,5 +136,27 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 20,
     fontSize: 18,
+  },
+  home: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  header: {
+    backgroundColor: 'orange',
+    width: 'auto',
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 18,
+    paddingTop: 25,
+  },
+  textHome: {
+    color: 'white',
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 50,
   }
+
 });
---
+++ src/data/content.js
@@ -0,0 +1,33 @@
+//Textos mocados para teste
+const content = {
+    title: 'Login',
+    subtitle: 'Welcome to LoginApp',
+    email: 'Email',
+    password: 'Password',
+    login: 'Login',
+    register: 'Create Account',
+    forgot: 'Forgot Password?',
+    name: 'Full Name',
+    home: 'Welcome',
+    logout: 'Logout',
+    footer: '© 2023 - All Rights Reserved',
+    footer2: 'Leonardo Vechieti',
+    error: 'Error',
+    errorEmail: 'Invalid email',
+    errorPassword: 'Invalid password',
+    errorName: 'Invalid name',
+    errorLogin: 'Invalid email or password',
+    errorRegister: 'Invalid email or password',
+    errorForgot: 'Invalid email',
+    errorLogout: 'Error on logout',
+    success: 'Success',
+    successLogin: 'Login success',
+    successRegister: 'Register success',
+    successForgot: 'Email sent',
+    successLogout: 'Logout success',
+    titleSite: "A cada desafio, uma nova solução",
+    contentSite: "Ajudamos nossos clientes a elevar seu potencial com soluções de tecnologia pensadas sob medida. Nossos times de especialistas constroem de forma estratégica as melhores experiências digitais para o que o mercado demanda e os negócios precisam.",
+    contenSite2: "Contamos com 6.000 profissionais de diferentes vivências que compartilham suas paixões e habilidades para alcançar resultados de excelência ao lado de nossos clientes, construindo juntos histórias incríveis."
+};
+
+export default content;
